d7739a12122327a2ae0673b0e2dc76e821cebed0
jomid314 <66674156+jomid314@users.noreply.github.com>
2023-03-27 06:27:26 +0800

    Added Contact form

--- pages/Contact_us.py
+++ pages/Contact_us.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.header("Contact Me")
 
+with st.form(key = "email_forms"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your Message")
+    button = st.form_submit_button("Submit")
+    if button:
+        print("I was pressed")
