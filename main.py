516d615a58d0a84d2f3d1773ab2786d13eb6e2b9
jomid314 <66674156+jomid314@users.noreply.github.com>
2023-03-26 17:55:27 +0800

    Placed columns in container and add description

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import streamlit as st
 
 st.set_page_config(layout='wide')
 
-col1, col2 = st.columns(2)
 
-with col1:
-    st.image("images/photo.png")
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("images/photo.png")
+    with col2:
+        st.title("Joe Smith")
+        content = """
+        Hi I am Joe
+        """
+        st.info(content)
 
-with col2:
-    st.title("Joe Smith")
-    content = """
-    Hi I am Joe
-    """
 
-    st.info(content)
+summary = """
+Below you can find some of the apps
+I have built in Python."""
+st.info(summary)
